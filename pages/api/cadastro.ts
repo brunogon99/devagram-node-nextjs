6fd1058278704699be40870d337110d94b18ab83
Bruno Gonçalves <b.goncalves484@gmail.com>
2022-03-09 16:12:21 -0300

    validacao de usuario logado, validacao de cadastro de usuario

---
+++ pages/api/cadastro.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
+import {UsuarioModel} from '../../models/UsuarioModel';
+import md5 from 'md5';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
+
+const endpointCadastro = 
+    async (req : NextApiRequest, res : NextApiResponse <RespostaPadraoMsg>) => {
+    if(req.method === 'POST'){
+        const usuario = req.body as CadastroRequisicao;
+
+        if(!usuario.nome || usuario.nome.length<2){
+            return res.status(400).json({erro: 'Nome inválido'});
+        }
+
+        if(!usuario.email || usuario.email.length < 5
+            || !usuario.email.includes('@')
+            || !usuario.email.includes('.')){
+                return res.status(400).json({erro: 'Email inválido'});
+            }
+        
+        if(!usuario.senha || usuario.senha.length < 4){
+            return res.status(400).json({erro: 'Senha inválida'});
+        }
+        //validacao se ja existe usuario com o mesmo email
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+            return res.status(400).json({erro: 'ja existe cadastro com o email informado'});
+        }
+
+        //Salvar no banco de dados
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuario);
+        return res.status(200).json({msg: 'Usuario criado com sucesso'});
+    }
+    return res.status(405).json({erro : 'Metodo informado não é válido'});
+}
+
+export default conectarMongoDB(endpointCadastro);
